2d14b11e5780da64b55965e04678927ab0cbdfa7
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-09-12 09:18:38 -0400

    Lab 1-2.py finished

---
+++ lab_1-2.py
@@ -0,0 +1,23 @@
+#V.K 9/9/2022
+
+#1. Create a Python file named lab_1-2.py
+
+#2. Add the heading to the first line of your file.
+
+#3. Create a variable and set it equal to your first name. Make sure the variable follows all the
+#variable naming rules.
+
+first_name = 'Vishnu'
+#4. Create a print statement to display your variable when your code is run.
+
+print(first_name)
+#5. Run the program.
+#6. Hit enter twice. Then hit Tab once or Space four times.
+
+#7. Create a variable and set it equal to "your favorite hobby". Make sure the variable follows all
+#the variable naming rules.
+
+favorite_hobby = 'Hiking'
+#8. Create a print statement to display your variable when your code is run. 9. What happens when you run the program again?
+
+print(favorite_hobby)
